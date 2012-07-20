e325831d4ccdcf5b30a7796a75baff5611aadd23
Will Stockwell <will@dropbox.com>
2012-07-20 15:28:16 -0700

    config tweaks

--- config.py
+++ config.py
@@ -1,41 +1,7 @@
 MODES = {
- "normal":
+ "dnb":
      {
         "channel" : 0,
-        "note" : {
-            "square" : 0,
-            "ex" : 2,
-            "circle" : 4,
-            "triangle" : 5,
-            "left" : 24,
-            "down" : 26,
-            "right" : 28,
-            "up" : 29,
-            "r1" : 36,
-            "l1" : 38,
-            "r2" : 49,
-            "l2" : 50,
-            "start" : 65,
-            "right_hat" : 66,
-            "left_hat" : 67,
-            },
-        "cc" : {
-            "left_analog_horiz" : 65,
-            "left_analog_vert" : 66,
-            "right_analog_horiz" : 67,
-            "right_analog_vert" : 68,
-            "accX" : 69,
-            "accY" : 70,
-            "accZ" : 71,
-            },
-        "pitch": [
-            ],
-        "pitch_two_side": [
-            ],
-        },
- "pitch":
-     {
-        "channel" : 1,
         "note" : {
             "square" : 0,
             "ex" : 2,
@@ -54,22 +20,22 @@ MODES = {
             "left_hat" : 43,
             },
         "cc" : {
-            "left_analog_horiz" : 65,
-            "left_analog_vert" : 66,
+            "right_analog_horiz" : 65,
+            "right_analog_vert" : 66,
             "accX" : 69,
             "accY" : 70,
             "accZ" : 71,
             },
         "pitch": [
-            "right_analog_horiz",
-            "right_analog_vert",
+            "left_analog_horiz",
+            "left_analog_vert",
             ],
         "pitch_two_side": [
             ],
         },
- "pitch2":
+ "lightsaber":
      {
-        "channel" : 2,
+        "channel" : 1,
         "note" : {
             "square" : 0,
             "ex" : 2,
@@ -79,26 +45,26 @@ MODES = {
             "down" : 26,
             "right" : 28,
             "up" : 29,
+            "r1" : 36,
             "l1" : 38,
+            "r2" : 49,
             "l2" : 50,
             "start" : 65,
-            "right_hat" : 66,
-            "left_hat" : 67,
+            "right_hat" : 21,
+            "left_hat" : 20,
             },
         "cc" : {
-            "left_analog_horiz" : 65,
-            "left_analog_vert" : 66,
-            "right_analog_horiz" : 67,
-            "right_analog_vert" : 68,
+            "right_analog_horiz" : 65,
+            "right_analog_vert" : 66,
             "accX" : 69,
             "accY" : 70,
             "accZ" : 71,
             },
         "pitch": [
+            "left_analog_horiz",
+            "left_analog_vert",
             ],
         "pitch_two_side": [
-            "r1",
-            "r2",
             ],
         },
  }
